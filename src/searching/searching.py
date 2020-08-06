ae84a4ac9ea75ac4d5d6373eb239223999ce8b00
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-08-05 21:20:28 -0500

    Adds Iterative Banry Search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -13,6 +13,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        middle = (left + right) // 2
+        if arr[middle] == target:
+            return middle
+        elif arr[middle] > target:
+            # search to the right
+            right = middle - 1
+        else:
+            # search to the left
+            left = middle + 1
 
 
     return -1  # not found
